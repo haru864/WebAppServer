81ca6200c040c3e040e7fdb9f23128cd2f40d48d
haru864 <kawachigenji95@gmail.com>
2022-05-15 02:24:45 +0900

    fix header component location

--- src/display/ClientApp.java
+++ src/display/ClientApp.java
@@ -2,7 +2,13 @@ package display;
 
 public class ClientApp {
 
+    public static String[] STATUS_LIST = new String[] { "Outage", "Running" }; // 0->停止中、1->稼働中
+    public static String SERVER_STATUS;
+
     public static void main(String[] args) {
+
+        // サーバーステータスの初期化
+        SERVER_STATUS = new String(STATUS_LIST[0]);
 
         // 操作画面を表示
         new ClientFrame();
--- src/display/ClientFrame.java
+++ src/display/ClientFrame.java
@@ -1,15 +1,80 @@
 package display;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.util.HexFormat;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import javax.swing.plaf.DimensionUIResource;
 
 public class ClientFrame {
 
+    public static int FONT_SIZE = 20;
+    public static int FRAME_WIDTH = 400;
+    public static int FRAME_HEIGHT = 500;
+
     public ClientFrame() {
 
+        // フレームを作成
         JFrame clientFrame = new JFrame("WebAppServer v1.0");
-        clientFrame.setPreferredSize(new Dimension(600, 450));
+        clientFrame.setPreferredSize(new Dimension(FRAME_HEIGHT, FRAME_WIDTH));
+        clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        clientFrame.setResizable(false);
+
+        // パネルを作成
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        JPanel statusPanel = new JPanel();
+        JPanel switchPanel = new JPanel();
+        JPanel logPanel = new JPanel();
+
+        // ラベルを作成
+        JLabel dummyLeft = new JLabel("        ");
+        JLabel dummyRight = new JLabel("        ");
+        JLabel label = new JLabel("Server Status: ");
+        JLabel status = new JLabel(ClientApp.SERVER_STATUS);
+        dummyLeft.setFont(new Font(Font.DIALOG, Font.PLAIN, FONT_SIZE * 3));
+        dummyRight.setFont(new Font(Font.DIALOG, Font.PLAIN, FONT_SIZE * 3));
+        label.setFont(new Font(Font.DIALOG, Font.PLAIN, FONT_SIZE));
+        status.setFont(new Font(Font.DIALOG, Font.PLAIN, FONT_SIZE));
+        ;
+
+        dummyLeft.setBorder(new LineBorder(Color.RED, 2, true));
+        dummyRight.setBorder(new LineBorder(Color.RED, 2, true));
+        label.setBorder(new LineBorder(Color.RED, 2, true));
+        status.setBorder(new LineBorder(Color.RED, 2, true));
+
+        statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.LINE_AXIS));
+        statusPanel.add(dummyLeft);
+        statusPanel.add(label);
+        statusPanel.add(status);
+        statusPanel.add(dummyRight);
+
+        statusPanel.setPreferredSize(new Dimension(FRAME_HEIGHT, FRAME_WIDTH / 3));
+        statusPanel.setBackground(Color.CYAN);
+
+        // ボタンを作成
+        JButton bootUpBtn = new JButton("Boot up");
+        JButton shutdownBtn = new JButton("Shutdown");
+        JButton logOutputBtn = new JButton("Log output");
+        switchPanel.add(bootUpBtn);
+        switchPanel.add(shutdownBtn);
+        logPanel.add(logOutputBtn);
+
+        mainPanel.add(statusPanel, BorderLayout.NORTH);
+        mainPanel.add(switchPanel, BorderLayout.WEST);
+        mainPanel.add(logPanel, BorderLayout.EAST);
+        clientFrame.add(mainPanel);
+
         clientFrame.pack();
         clientFrame.setLocationRelativeTo(null);
         clientFrame.setVisible(true);
